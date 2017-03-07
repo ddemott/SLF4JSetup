83cb075af3c5606b379d99b152e05a60cfd65bba
Dale DeMott <daledemott@gmail.com>
2017-03-07 13:43:07 -0600

    Initial commit.

---
+++ src/com/demott/Hello.java
@@ -0,0 +1,16 @@
+package com.demott;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hello {
+    static Logger LOGGER = (Logger) LoggerFactory.getLogger(Hello.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            if (i % 2 == 0)
+                LOGGER.info("Hello {}", i);
+            else
+                LOGGER.debug("I am on index {}", i);
+    }
+}
